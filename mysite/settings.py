dc823ec33e282a0f9590c27874cb5108a3e6da46
lintao <lintao@qq.com>
2019-03-15 18:58:27 +0800

    init

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myblog',
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+TIME_ZONE = 'Asia/Shanghai'
